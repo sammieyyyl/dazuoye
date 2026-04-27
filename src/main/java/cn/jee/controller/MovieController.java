89d885b3a6959e3154a7447b3dcae068eb9d59ec
sammie <3369892307@qq.com>
2026-04-27 18:52:06 +0800

    feat: 新增电影功能

--- src/main/java/cn/jee/controller/MovieController.java
+++ src/main/java/cn/jee/controller/MovieController.java
@@ -1,17 +1,25 @@
 package cn.jee.controller;
 
+import cn.jee.entity.Movie;
 import cn.jee.repository.MovieRepository;
+import cn.jee.repository.UserRepository;
 import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping("/movie")
 public class MovieController {
   @Autowired
   MovieRepository movieRepository;
+
+  @Autowired
+  UserRepository userRepository;
 
   @RequestMapping("/list")
   public String list(Model model, HttpSession session) {
@@ -24,5 +32,34 @@ public class MovieController {
     model.addAttribute("movies", movies);
     model.addAttribute("username", username);
     return "movie/list";
+  }
+
+  @RequestMapping("/new")
+  public String newPage(Model model, HttpSession session) {
+    Object loginUser = session.getAttribute("login_user");
+    if (loginUser == null) {
+      return "redirect:/";
+    }
+    model.addAttribute("movie", new Movie());
+    return "movie/new";
+  }
+
+  @RequestMapping(value = "/save", method = RequestMethod.POST)
+  public String save(@Valid Movie movie, BindingResult bindingResult, HttpSession session) {
+    Object loginUser = session.getAttribute("login_user");
+    if (loginUser == null) {
+      return "redirect:/";
+    }
+    if (bindingResult.hasErrors()) {
+      return "movie/new";
+    }
+    String username = loginUser.toString();
+    var userOpt = userRepository.findByName(username);
+    if (userOpt.isEmpty()) {
+      return "redirect:/";
+    }
+    movie.setUser(userOpt.get());
+    movieRepository.save(movie);
+    return "redirect:/movie/list";
   }
 }
