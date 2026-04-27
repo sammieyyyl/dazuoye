8a25fe7f7ed3af6befd2ae793449fc406665a947
sammie <3369892307@qq.com>
2026-04-27 18:06:16 +0800

    feat: 电影实体与校验规则

--- src/main/java/cn/jee/entity/Movie.java
+++ src/main/java/cn/jee/entity/Movie.java
@@ -1,6 +1,8 @@
 package cn.jee.entity;
 
+import jakarta.persistence.CollectionTable;
 import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -8,8 +10,16 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 @Getter
 @Setter
@@ -20,16 +30,44 @@ public class Movie {
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
+  @NotBlank
+  @Size(min = 10, max = 10)
   @Column(unique = true, nullable = false, length = 10)
   private String watchTime;
 
   private String name;
 
+  @NotNull
+  @Positive(message = "{movie.price.positive}")
   private Double price;
 
+  @NotBlank
+  @Size(min = 20, message = "{movie.comment.min}")
   private String comment;
+
+  @ElementCollection
+  @CollectionTable(name = "movie_image", joinColumns = @JoinColumn(name = "movie_id"))
+  @Column(name = "image_name")
+  private List<String> images = new ArrayList<>();
 
   @ManyToOne
   @JoinColumn(name = "user_id")
   private User user;
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    Movie movie = (Movie) o;
+    return Objects.equals(watchTime, movie.watchTime);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(watchTime);
+  }
 }
--- src/main/java/cn/jee/entity/User.java
+++ src/main/java/cn/jee/entity/User.java
@@ -5,9 +5,13 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @Setter
@@ -20,4 +24,7 @@ public class User {
 
   @Column(unique = true, nullable = false)
   private String name;
+
+  @OneToMany(mappedBy = "user")
+  private List<Movie> movies = new ArrayList<>();
 }
